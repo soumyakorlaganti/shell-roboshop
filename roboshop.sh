1d70c82b8591ed9b89a8e5c3ce2eb8c8d4af7e64
soumya <korlagani.soumya@gmail.com>
2026-02-04 12:20:01 +0530

    creating server and get ip address

---
+++ roboshop.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+SG_ID="sg-0a4e2040f42d9d840"  # replace with your ID
+AMI_ID="ami-0220d79f3f480ecf5"
+
+for instance in $@
+do
+    INSTANCE_ID=$( aws ec2 run-instances \
+    --image-id $AMI_ID \
+    --instance-type "t3.micro"
+    --security-group-ids $SG_ID \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+    --query 'Instances[0].InstanceId' \
+    --output text )
+    
+    if [ $instance == "frontend" ]; then
+        IP=$(
+            aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query 'Reservations[].Instances[].PrivateIpAddress' \
+            --output text
+        )
+    else
+        IP=$(
+            aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query 'Reservations[].Instances[].PrivateIpAddress' \
+            --output text
+        )
+    f1
+
+    echo "IP Address: $IP"
+done
