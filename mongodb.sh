222bf16b907db4aaf4719c8a25d4319dda2a0b0b
soumya <korlagani.soumya@gmail.com>
2026-02-04 14:35:58 +0530

    creating mongoDB

--- mongodb.sh
+++ mongodb.sh
@@ -44,5 +44,6 @@ VALIDATE $? "Start MongoDB"
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
 VALIDATE $? "Allowing remote connections"
 
+
 systemctl restart mongod
 VALIDATE $? "Restarted MongoDB"
